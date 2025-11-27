b82c8b4f1868b192eb32c310f8a69e04c85abf9a
Henrico Birochi <henrico.birochi@gmail.com>
2025-11-27 11:40:12 -0300

    feat: Implement PeopleRepository with insert and get methods

---
+++ tests/repositories/repositories_test.py
@@ -0,0 +1,40 @@
+import pytest
+from src.models.sqlite.settings.connection import db_connection_handler
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+from src.models.sqlite.repositories.people_repository import PeopleRepository
+
+# db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Connecting to Data Base")
+def test_list_pets():
+    repo = PetsRepository(db_connection_handler)
+    response = repo.list_pets()
+    print()
+    print(response)
+
+@pytest.mark.skip(reason="Connecting to Data Base")
+def test_delete_pets():
+    name = "belinha"
+
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
+
+@pytest.mark.skip(reason="Connecting to Data Base")
+def test_insert_people():
+    first_name = "test name"
+    last_name = "test name"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason="Connecting to Data Base")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print(response)
+    print(response.pet_name)
